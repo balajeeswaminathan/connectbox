f4bf256c1f4e82f0a22cad6176c07f332cbb59e1
balajee <balajee@skava.com>
2018-02-13 14:07:40 +0530

    mongo version

--- src/main/java/com/smp/utils/Util.java
+++ src/main/java/com/smp/utils/Util.java
@@ -11,8 +11,8 @@ public class Util {
 	{
 		try
 		{
-			//MongoClientURI uri = new MongoClientURI("mongodb://smpadmin:manihigher@cluster0-shard-00-00-iisbh.mongodb.net:27017,cluster0-shard-00-01-iisbh.mongodb.net:27017,cluster0-shard-00-02-iisbh.mongodb.net:27017/smp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
-			MongoClientURI uri = new MongoClientURI("mongodb+srv://smpadmin:manihigher@cluster0-iisbh.mongodb.net/test");
+			MongoClientURI uri = new MongoClientURI("mongodb://smpadmin:manihigher@cluster0-shard-00-00-iisbh.mongodb.net:27017,cluster0-shard-00-01-iisbh.mongodb.net:27017,cluster0-shard-00-02-iisbh.mongodb.net:27017/smp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
+			//MongoClientURI uri = new MongoClientURI("mongodb+srv://smpadmin:manihigher@cluster0-iisbh.mongodb.net/test");
 			MongoClient mongoClient;
 			mongoClient = new MongoClient(uri);
 			return mongoClient.getDB("smp");
